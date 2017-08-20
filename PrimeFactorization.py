16d0d3706eae74be2817046eb4ff25c7638b7fd8
Tim <timothyjames.short@gmail.com>
2017-08-20 15:35:36 -0400

    Fixed prime factorization

--- PrimeFactorization.py
+++ PrimeFactorization.py
@@ -1,12 +1,24 @@
 #Given a number
 #Find the prime factorization
 
+from numpy import prod
+import time
+
 def findPrimeFactor(n):
-	
+	for primeFactor in range(2,(n/2+1)):
+		if (n % primeFactor == 0):
+			return primeFactor
+	return n
 
-n = 150
-primeFactors = []
-primeFactor = 0
-while (primeFactor != 1):
-	for primeFactor in range(2,n/2):
+number = 5**12
+primeFactors = [1]
 
+start = time.time()
+
+while (findPrimeFactor(number/prod(primeFactors)) != 1):
+	primeFactors+= [findPrimeFactor(number/prod(primeFactors))]
+
+elapsed = time.time() - start
+
+print primeFactors
+print str(elapsed) + " elapsed time"
