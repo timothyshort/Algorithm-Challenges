53b112ee0ff6434d30679e69c743397271fa6f63
Tim <timothyjames.short@gmail.com>
2017-08-20 15:14:47 -0400

    Added prime factorization

---
+++ PrimeFactorization.py
@@ -0,0 +1,12 @@
+#Given a number
+#Find the prime factorization
+
+def findPrimeFactor(n):
+	
+
+n = 150
+primeFactors = []
+primeFactor = 0
+while (primeFactor != 1):
+	for primeFactor in range(2,n/2):
+
