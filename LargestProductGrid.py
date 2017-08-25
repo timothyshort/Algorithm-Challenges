f79e450f718a75f6a5f278cbe67e79ba3dec2358
Tim <timothyjames.short@gmail.com>
2017-08-25 09:34:41 -0400

    Fixed and optimized largest product

--- LargestProductGrid.py
+++ LargestProductGrid.py
@@ -1,34 +1,31 @@
+import time
 
-def maxHorz(data):
-	#rowData = [sum(data[0][i:i+4]) for i in range(0, cols - 3)]
-	#rowData = [data[0][j]*data[0][j+1]*data[0][j+2]*data[0][3] for j in range(0, cols - 3)]
+def maxHorz(data,maxData=0):
 
 	for i in range(len(data)):
 		rowData = [data[i][j]*data[i][j+1]*data[i][j+2]*data[i][j+3] for j in range(0, len(data[0]) - 3)]
-		print "ROW " + str(i+1) + str(rowData)
-	return max(rowData)
+		maxData = max(max(rowData),maxData)
+	return maxData
 
-def maxVert(data):
+def maxVert(data,maxData=0):
 	for j in range(len(data)):
 		rowData = [data[i][j]*data[i+1][j]*data[i+2][j]*data[i+3][j] for i in range(0, len(data[0])-3)]
-		print "COL " + str(j+1) + str(rowData)
-	return max(rowData)
+		maxData = max(max(rowData),maxData)
+	return maxData
 
-def maxLowerDiag(data):
+def maxLowerDiag(data,maxData=0):
 	#Find each 4-wise downward diagonal in each row
 	for i in range(len(data)-3):
 		rowData = [data[i][j]*data[i+1][j+1]*data[i+2][j+2]*data[i+3][j+3] for j in range(0, len(data[0]) - 3)]
-		print "LOWER DIAG \ " + str(i+1) + str(rowData)
-	return max(rowData)
+		maxData = max(max(rowData),maxData)
+	return maxData
 
-def maxUpperDiag(data):
+def maxUpperDiag(data,maxData=0):
 	#Find each 4-wise upward diagonal in each row
 	for i in range(3,len(data)):
 		rowData = [data[i][j]*data[i-1][j+1]*data[i-2][j+2]*data[i-3][j+3] for j in range(0, len(data[0])-3)]
-		print "UPPER DIAG / " + str(i+1) + str(rowData)
-	return max(rowData)
-
-
+		maxData = max(max(rowData),maxData)
+	return maxData
 
 def getData():
 	rows = []
@@ -38,15 +35,12 @@ def getData():
 	return rows
 
 data = getData()
-print data; print ""
-print "ROWS: " + str(len(data))
-print "COLS: " + str(len(data[0]))
 
-print max(maxHorz(data),maxVert(data),maxLowerDiag(data),maxUpperDiag(data))
-"""
-m1 = maxHorz(data)
-m2 = maxVert(data)
-m3 = maxLowerDiag(data)
-m4 = maxUpperDiag(data)
-print max(m1,m2,m3,m4)
-"""
+start = time.time()
+maxProduct = max(maxHorz(data),maxVert(data),maxLowerDiag(data),maxUpperDiag(data))
+elapsed = time.time()-start
+
+print "Largest 4-Term Product in Grid:",
+print maxProduct
+print "Elapsed Time:",
+print elapsed
