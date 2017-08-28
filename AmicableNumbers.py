3ed2c34365cdb8d8c92644f65e90e5a0a480ff0d
Tim <timothyjames.short@gmail.com>
2017-08-28 10:18:38 -0400

    Initial solution to amicable numbers

---
+++ AmicableNumbers.py
@@ -0,0 +1,22 @@
+import time
+
+
+max = 10000
+sumAmicableNumbers=0
+list = [1] * (max)
+
+start = time.time()
+
+#Start at 2 and go until square root of n
+#We only need to go the square root because for each a * b = n, we don't need b * a
+for a in range(2, int (max**.5) +1):
+	#Now find the sum of i and j where i * j = n
+	for b in range (a, int (max/a)+1):
+		list[a * b - 1] += a + b
+
+elapsed = time.time() - start
+
+print "Sum of Amicable Numbers 1 to " + str(max) + ":"
+print sum(list)
+print "Time Elapsed:"
+print elapsed
