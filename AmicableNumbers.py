dd10a6a2c655f90c44e0be9365e1246d9cf4e263
Tim <timothyjames.short@gmail.com>
2017-08-28 11:03:33 -0400

    Fixed amicable numbers algorithm

--- AmicableNumbers.py
+++ AmicableNumbers.py
@@ -13,12 +13,21 @@ for a in range(2, int (max**.5) +1):
 	for b in range (a, int (max/a)+1):
 		list[a * b - 1] += a + b
 
+#Now test if d(i)=j and d(j)=i
+sums=0
+for i in range(2,max):
+	for j in range(i+1, max):
+		if (list[i] == j+1 and list[j] == i+1):
+			print i+1, list[i]
+			print j+1, list[j]
+			sums+= i+1 + j+1
+
 elapsed = time.time() - start
 
 print "Sum of Amicable Numbers 1 to " + str(max) + ":"
-print sum(list)
+print sums
 print "Time Elapsed:"
 print elapsed
 
-n = int(input("Find sum of amicable number: "))
-print list[n-1]
+#n = int(input("Find sum of amicable number: "))
+#print list[n-1]
