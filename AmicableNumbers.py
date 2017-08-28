1e078d228bded883947e01d8fd264451d339b1e7
Tim <timothyjames.short@gmail.com>
2017-08-28 11:45:56 -0400

    Fixed amicable numbers algorithm and simplified code

--- AmicableNumbers.py
+++ AmicableNumbers.py
@@ -1,33 +1,31 @@
 import time
 
 max = int(input("Enter maximum number:"))
-sumAmicableNumbers=0
 list = [1] * (max)
 
-start = time.time()
+t1 = time.time()
 
 #Start at 2 and go until square root of n
 #We only need to go the square root because for each a * b = n, we don't need b * a
 for a in range(2, int (max**.5) +1):
+	#Sieve method
 	#Now find the sum of i and j where i * j = n
 	for b in range (a, int (max/a)+1):
 		list[a * b - 1] += a + b
 
+t2 = time.time()
+
 #Now test if d(i)=j and d(j)=i
-sums=0
+sumAmicableNumbers=0
 for i in range(2,max):
-	for j in range(i+1, max):
-		if (list[i] == j+1 and list[j] == i+1):
-			print i+1, list[i]
-			print j+1, list[j]
-			sums+= i+1 + j+1
+	if list[i] < max and list[i] < i:
+		j = list[i]
+		if (list[j-1] == i+1) :
+			sumAmicableNumbers+= i+1 +j
 
-elapsed = time.time() - start
+t3 = time.time()
 
-print "Sum of Amicable Numbers 1 to " + str(max) + ":"
-print sums
-print "Time Elapsed:"
-print elapsed
-
-#n = int(input("Find sum of amicable number: "))
-#print list[n-1]
+print("Sum of Amicable Numbers 1 to %s: %s") % (max, sumAmicableNumbers)
+print("%s time elapsed to find Product Pairs") % (t2-t1)
+print("%s time elapsed to find Amicable Number Sums") % (t3-t2)
+print("%s total elapsed time") % (t3-t1)
