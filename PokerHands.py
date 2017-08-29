f61ceb4e0755ab39aa071dcb4dc9991f461e5d28
Tim <timothyjames.short@gmail.com>
2017-08-29 11:14:29 -0400

    Determined best hand and outlinefor data comparisons

--- PokerHands.py
+++ PokerHands.py
@@ -1,7 +1,8 @@
-hands = ["1H", "4H", "4H", "1H", "2H"]
+hands = ["5D", "3H", "2H", "2S", "4S"]
 handsNums = [0]*5
 handsSuit = [0]*5
 
+#Convert data to value
 def num_to_value(card):
 	print card,
 	if card == "T" : return 10
@@ -10,7 +11,6 @@ def num_to_value(card):
 	elif card == "K" : return 13
 	elif card == "A" : return 14
 	else: return int(card)
-
 def suit_to_value(card):
 	print card,
 	if card == "C" : return 1
@@ -18,33 +18,28 @@ def suit_to_value(card):
 	elif card == "D" : return 3
 	elif card == "H" : return 4
 
-
-
+#Winning Hands
 def is_straight(hand):
 	if hand == [hand[0],hand[0]+1,hand[0]+2,hand[0]+3,hand[0]+4]:
 		print "Straight"
 		return True
-
 def is_flush(hand):
 	clubs = [1,1,1,1,1]
 	spades = [2,2,2,2,2]
 	diamonds = [3,3,3,3,3]
 	hearts = [4,4,4,4,4]
 	if (hand == clubs or hand == spades or hand == diamonds or hand == hearts):
-		return True
-
+		return "Flush"
 def is_royal_flush(hand):
 	royal = [10,11,12,13,14]
 	if sum(hand) % sum(royal) == 0 :
 		return True
-
 def is_four_of_a_kind(hand):
 	if (hand[0] == hand[1] or hand[0] == hand[2]):
 		test = hand[0]
 	elif (hand[1] == hand[0] or hand[1] == hand[2]):
 		test = hand[1]
 	else: return False
-
 	count=0
 	for i in range(0,5):
 		if hand[i] == test:
@@ -52,14 +47,12 @@ def is_four_of_a_kind(hand):
 	if count == 4:
 		return True
 	return False
-
 def is_full_house(hand):
 	card1, card2 = hand[0], 0
 	cardCount1,	cardCount2 = 0,0
 	card2 = 0
 	for i in range(0,5):
-		if hand[i] == card1:
-			cardCount1+=1
+		if hand[i] == card1: cardCount1+=1
 		else:
 			card2 = hand[i]
 			cardCount2+=1
@@ -73,14 +66,12 @@ def is_full_house(hand):
 	if (cardCount1 == 3 and cardCount2 == 2) or (cardCount1 == 2 and cardCount2 == 3):
 		return True
 	return False
-
 def is_pair(hand):
 	for i in range(0,5):
 		test = hand[i]
 		for j in range(i+1,5):
 			if hand[j] == test:
 				return True
-
 def is_three_of_a_kind(hand):
 	card1 = hand[0]
 	card2,card3 = 0,0
@@ -106,7 +97,6 @@ def is_three_of_a_kind(hand):
 	if cardCount1 == 3 or cardCount2 == 3 or cardCount3 == 3:
 		return True
 	return False
-
 def is_two_pair(hand):
 	card1 = hand[0]
 	card2,card3 = 0,0
@@ -129,47 +119,64 @@ def is_two_pair(hand):
 		elif hand[k] == card2 : cardCount2+=1
 		elif hand[k] == card3 : cardCount3+=1
 		else: return False
-	print cardCount1,
-	print cardCount2,
-	print cardCount3
 	if (cardCount1==2 and cardCount2==2) or (cardCount1==2 and cardCount3==2) or (cardCount2==2 and cardCount3==2):
 		return True
 	return False
 
+#Test for Straight, Straight Flush, Royal Flush
+def is_ordered(handNums,handSuit):
+	if is_straight(handNums):
+		if is_flush(handSuit):
+			if is_royal_flush(handNums):
+				print "Royal Flush"
+				return 10
+			else:
+				print "Straight Flush"
+				return 9
+		else:
+			print "Straight"
+			return 6
+
+#Test for pairs
+def is_paired(handNums):
+	if is_pair(handNums):
+		if is_three_of_a_kind(handNums):
+			if is_full_house(handNums):
+				print "Full House"
+				return 7
+			print "Three of a kind"
+			return 5
+		if is_four_of_a_kind(handNums):
+			print "Four of a Kind"
+			return 8
+		if is_two_pair(handNums):
+			print "Two pair"
+			return 4
+		print "Pair"
+		return 1
 
 
-#Convert data to readable format
+#Program Outline
+p1Wins,p2Wins = 0,0
+
+#1 READ THE DATA
+
+#2 CONVERT DATA TO READABLE FORMAT
 for i in range(0,5):
 	handsNums[i] = num_to_value(hands[i][0])
 	handsSuit[i] = suit_to_value(hands[i][1])
 print ""
 
+#3 FIND WINNING HAND
+player1 = max(is_ordered(handsNums,handsSuit), is_paired(handsNums), is_flush(handsSuit))
+player2 = max(is_ordered(handsNums,handsSuit), is_paired(handsNums), is_flush(handsSuit))
 
-#Test for Straight, Straight Flush, Royal Flush
-if is_straight(handsNums):
-	if is_flush(handsSuit):
-		if is_royal_flush(handsNums):
-			print "Royal Flush"
-		else: print "Straight Flush"
-	else: print "Straight"
-
-#Test for pairs
-if is_pair(handsNums):
-	print "Pair"
-	if is_two_pair(handsNums):
-		print "Two pair"
-	if is_three_of_a_kind(handsNums):
-		print "Three of a Kind"
-	#is_four_of_a_kind(handsNums)
-	#is_full_house
-
-if is_four_of_a_kind(handsNums):
-	print "Four of a Kind"
-
-if is_full_house(handsNums):
-	print "Full House"
-
-if is_flush(handsSuit):
-	print "Flush"
-
-#Set up first, then optimize later
+#4 COMPARE WINNING HAND
+if player1 > player2:
+	print "PLAYER 1 WINS"
+	p1Wins+=1
+elif player2 > player1 :
+	print "PLAYER 2 WINDS"
+	p2Wins+=1
+else:
+	#TIE GOES TO HIGHEST CARD
