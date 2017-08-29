4834726637112696584c6d1a8c88240d2be4c2da
Tim <timothyjames.short@gmail.com>
2017-08-29 10:20:53 -0400

    Implemented winning hands

--- PokerHands.py
+++ PokerHands.py
@@ -1,4 +1,4 @@
-hands = ["2H", "1H", "4H", "4H", "AH"]
+hands = ["1H", "4H", "4H", "1H", "2H"]
 handsNums = [0]*5
 handsSuit = [0]*5
 
@@ -81,11 +81,69 @@ def is_pair(hand):
 			if hand[j] == test:
 				return True
 
+def is_three_of_a_kind(hand):
+	card1 = hand[0]
+	card2,card3 = 0,0
+	cardCount1,cardCount2,cardCount3 = 0,0,0
+	for i in range(0,5):
+		if hand[i] == card1 : cardCount1+=1
+		else:
+			card2 = hand[i]
+			cardCount2+=1
+			break
+	for j in range(i+1,5):
+		if hand[j] == card1 : cardCount1+=1
+		elif hand[j] == card2 : cardCount2+=1
+		else:
+			card3 = hand[j]
+			cardCount3+=1
+			break
+	for k in range(j+1,5):
+		if hand[k] == card1 : cardCount1+=1
+		elif hand[k] == card2 : cardCount2+=1
+		elif hand[k] == card3 : cardCount3+=1
+		else: return False
+	if cardCount1 == 3 or cardCount2 == 3 or cardCount3 == 3:
+		return True
+	return False
+
+def is_two_pair(hand):
+	card1 = hand[0]
+	card2,card3 = 0,0
+	cardCount1,cardCount2,cardCount3 = 0,0,0
+	for i in range(0,5):
+		if hand[i] == card1 : cardCount1+=1
+		else:
+			card2 = hand[i]
+			cardCount2+=1
+			break
+	for j in range(i+1,5):
+		if hand[j] == card1 : cardCount1+=1
+		elif hand[j] == card2 : cardCount2+=1
+		else:
+			card3 = hand[j]
+			cardCount3+=1
+			break
+	for k in range(j+1,5):
+		if hand[k] == card1 : cardCount1+=1
+		elif hand[k] == card2 : cardCount2+=1
+		elif hand[k] == card3 : cardCount3+=1
+		else: return False
+	print cardCount1,
+	print cardCount2,
+	print cardCount3
+	if (cardCount1==2 and cardCount2==2) or (cardCount1==2 and cardCount3==2) or (cardCount2==2 and cardCount3==2):
+		return True
+	return False
+
+
+
 #Convert data to readable format
 for i in range(0,5):
 	handsNums[i] = num_to_value(hands[i][0])
 	handsSuit[i] = suit_to_value(hands[i][1])
 print ""
+
 
 #Test for Straight, Straight Flush, Royal Flush
 if is_straight(handsNums):
@@ -95,10 +153,13 @@ if is_straight(handsNums):
 		else: print "Straight Flush"
 	else: print "Straight"
 
+#Test for pairs
 if is_pair(handsNums):
 	print "Pair"
-	#is_two_pair
-	#is_three_of_a_kind(handsNums)
+	if is_two_pair(handsNums):
+		print "Two pair"
+	if is_three_of_a_kind(handsNums):
+		print "Three of a Kind"
 	#is_four_of_a_kind(handsNums)
 	#is_full_house
 
