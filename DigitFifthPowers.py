7d351913217b44aeb5b09174d4f2534009006d1b
Tim <timothyjames.short@gmail.com>
2017-08-31 12:51:54 -0400

    Updated to include numbers less than 5 digits

--- DigitFifthPowers.py
+++ DigitFifthPowers.py
@@ -1,21 +1,16 @@
 import time
 
-def num_to_sum(a,b,c,d,e):
-	sum = str(a) + str(b) + str(c) + str(d) + str(e)
-	return int(sum)
-
 sum=0
-
 start = time.time()
 
-for a in range(1,10):
+for a in range(0,10):
 	for b in range(0,10):
 		for c in range(0,10):
 			for d in range(0,10):
 				for e in range(0,10):
-					if ([a,b,c,d,e] == [1,1,1,1,1]): pass
-					if num_to_sum(a,b,c,d,e) == a**5 + b**5 + c**5 + d**5 + e**5 :
-						prod= a**5 + b**5 + c**5 + d**5 + e**5
+					if (a+b+c+d+e == 0) or (a+b+c+d+e == 1): continue
+					if int (str(a) + str(b) + str(c) + str(d) + str(e)) == a**5 + b**5 + c**5 + d**5 + e**5 :
+						prod = a**5 + b**5 + c**5 + d**5 + e**5
 						print prod,
 						print "=",
 						print a,
