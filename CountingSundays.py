6bf4d842b5658ef2be68ebf188b3c33a6ccf3a36
Tim <timothyjames.short@gmail.com>
2017-09-06 09:03:28 -0400

    Optimized counting Sundays with list comprehension

--- CountingSundays.py
+++ CountingSundays.py
@@ -1,9 +1,5 @@
 import time
 
-min, max = 1901, 2000
-days, months = [], []
-numOfFirstMonthSundays=0
-
 #Add the index where month begins
 def addMonths(a):
 			months=[0]*(365+a)
@@ -19,36 +15,33 @@ def addMonths(a):
 			months[272+a] = 1
 			months[302+a] = 1
 			months[333+a] = 1
-			return months
+			return months	
+
+def findSundays():
+	min, max = 1900, 2000
+	days, months = [], []
+
+	#Make an array of each day
+	for n in range(min,max+1):
+			if (n % 4 == 0):
+				days+= [0]*366
+				months+= addMonths(1)
+			else:
+				days+= [0]*365
+				months+= addMonths(0)
+
+	#Find the whole number of weeks (floor)
+	weeks = int (len(days) / 7)
+
+	#Sunday is day 7 of the week,	use sieve method
+	days[6:(weeks*7):7] = [1]*(weeks)
+
+	#Using LC method
+	firstMonthSundays = [x for x in range(len(days[365:])) if (days[x] == 1 and months[x] == 1)]
+	return len(firstMonthSundays)
 
 start = time.time()
-
-#Make an array of each day
-for n in range(min,max+1):
-		if (n % 4 == 0):
-			days+= [0]*366
-			months+= addMonths(1)
-		else:
-			days+= [0]*365
-			months+= addMonths(0)
-
-#Find the whole number of weeks (floor)
-weeks = int (len(days) / 7)
-
-#Sunday is day 7 of the week
-#Using sieve method, make all multiples of 7s equal to 1
-days[6:(weeks*7):7] = [1]*(weeks)
-
-for x in range(len(days)):
-	if months[x] == 1 and days[x] == 1:
-		numOfFirstMonthSundays+=1
-
+ans = findSundays()
 elapsed = time.time() - start
 
-print "Days",
-print len(days)
-print "Weeks",
-print weeks
-print "Numer of Sundays that are the first day of the month:",
-print numOfFirstMonthSundays
-print "%s elapsed" %(elapsed)
+print "%s found in %s elapsed" %(ans, elapsed)
